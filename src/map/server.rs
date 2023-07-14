6d6392aad6b611e244269886448bf963d49172ab
marceline-cramer <cramermarceline@gmail.com>
2023-07-13 19:49:27 -0600

    map: def neighbor components and stitch tiles and chunks

--- src/map/server.rs
+++ src/map/server.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use ambient_api::prelude::*;
 use flowerpot::CHUNK_SIZE;
 
@@ -6,26 +8,56 @@ use messages::{LoadChunk, OnPlayerLoadChunk, Ready};
 
 mod shared;
 
+pub fn stitch_neighbors(entities: HashMap<IVec2, EntityId>) {
+    for (pos, e) in entities.iter() {
+        let pos = *pos;
+        let e = *e;
+
+        if let Some(n) = entities.get(&(pos + IVec2::X)).copied() {
+            entity::add_component(e, east_neighbor(), n);
+            entity::add_component(n, west_neighbor(), e);
+        }
+
+        if let Some(n) = entities.get(&(pos + IVec2::Y)).copied() {
+            entity::add_component(e, south_neighbor(), n);
+            entity::add_component(n, north_neighbor(), e);
+        }
+    }
+}
+
 #[main]
 pub fn main() {
     shared::init_shared_map();
 
+    let mut chunks = HashMap::new();
+    let mut tiles = HashMap::new();
     for x in -8..=8 {
         for y in -8..=8 {
             let position = IVec2::new(x, y);
 
             let tile_num = CHUNK_SIZE * CHUNK_SIZE;
-            let mut tiles = Vec::with_capacity(tile_num);
-            for _ in 0..tile_num {
-                tiles.push(Entity::new().spawn());
+            let mut chunk_tiles = Vec::with_capacity(tile_num);
+            for ty in 0..CHUNK_SIZE {
+                for tx in 0..CHUNK_SIZE {
+                    let tile = Entity::new().spawn();
+                    chunk_tiles.push(tile);
+
+                    let position = position * CHUNK_SIZE as i32 + ivec2(tx as i32, ty as i32);
+                    tiles.insert(position, tile);
+                }
             }
 
-            Entity::new()
+            let chunk = Entity::new()
                 .with(chunk(), position)
-                .with(chunk_tile_refs(), tiles)
+                .with(chunk_tile_refs(), chunk_tiles)
                 .spawn();
+
+            chunks.insert(position, chunk);
         }
     }
+
+    stitch_neighbors(chunks);
+    stitch_neighbors(tiles);
 
     let all_chunks = query(chunk()).build();
     Ready::subscribe(move |source, _| {
