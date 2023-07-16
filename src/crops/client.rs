758dfa45515b9bf49c3e048eb3b37296190fc640
marceline-cramer <cramermarceline@gmail.com>
2023-07-15 23:34:38 -0600

    crops: wait for updates' chunks to load

--- src/crops/client.rs
+++ src/crops/client.rs
@@ -25,39 +25,55 @@ fn main() {
     UpdateMediumCrops::subscribe({
         let chunks = chunks.clone();
         move |_, data| {
-            let chunks = chunks.read().unwrap();
-            let chunk_pos = data.chunk;
-            let Some(chunk) = chunks.get(&chunk_pos).copied() else { return };
-            let Some(tiles) = entity::get_component(chunk, chunk_tile_refs()) else { return };
-            for (tile_idx, class) in data
-                .crop_tiles
-                .into_iter()
-                .zip(data.crop_classes.into_iter())
-            {
-                let tile = tiles[tile_idx as usize];
-
-                if let Some(old_occupant) = entity::get_component(tile, medium_crop_occupant()) {
-                    if !old_occupant.is_null() {
-                        entity::despawn_recursive(old_occupant);
+            let chunks = chunks.clone();
+            run_async(async move {
+                let mut tries = std::iter::repeat(()).take(20);
+                let chunk = loop {
+                    if let Some(_) = tries.next() {
+                        if let Some(chunk) = chunks.read().unwrap().get(&data.chunk).copied() {
+                            break Some(chunk);
+                        }
+                    } else {
+                        break None;
                     }
+
+                    sleep(0.1).await;
+                };
+
+                let Some(chunk) = chunk else { return };
+                let Some(tiles) = entity::get_component(chunk, chunk_tile_refs()) else { return };
+
+                for (tile_idx, class) in data
+                    .crop_tiles
+                    .into_iter()
+                    .zip(data.crop_classes.into_iter())
+                {
+                    let tile = tiles[tile_idx as usize];
+
+                    if let Some(old_occupant) = entity::get_component(tile, medium_crop_occupant())
+                    {
+                        if !old_occupant.is_null() {
+                            entity::despawn_recursive(old_occupant);
+                        }
+                    }
+
+                    let occupant_position = (data.chunk * CHUNK_SIZE as i32).as_vec2()
+                        + vec2(
+                            (tile_idx as usize % CHUNK_SIZE) as f32,
+                            (tile_idx as usize / CHUNK_SIZE) as f32,
+                        )
+                        + 0.5;
+
+                    let new_occupant = entity::get_all_components(class)
+                        .with_default(medium_crop())
+                        .with(position(), occupant_position)
+                        .with(in_chunk(), chunk)
+                        .with(chunk_tile_index(), tile_idx)
+                        .spawn();
+
+                    entity::add_component(tile, medium_crop_occupant(), new_occupant);
                 }
-
-                let occupant_position = (chunk_pos * CHUNK_SIZE as i32).as_vec2()
-                    + vec2(
-                        (tile_idx as usize % CHUNK_SIZE) as f32,
-                        (tile_idx as usize / CHUNK_SIZE) as f32,
-                    )
-                    + 0.5;
-
-                let new_occupant = entity::get_all_components(class)
-                    .with_default(medium_crop())
-                    .with(position(), occupant_position)
-                    .with(in_chunk(), chunk)
-                    .with(chunk_tile_index(), tile_idx)
-                    .spawn();
-
-                entity::add_component(tile, medium_crop_occupant(), new_occupant);
-            }
+            });
         }
     });
 
