50dd01e028a800c808e4e5b0459a49b53d77657b
marceline-cramer <cramermarceline@gmail.com>
2023-07-17 17:23:38 -0600

    items: held_ref + network sync + model spawning

--- src/items/client.rs
+++ src/items/client.rs
@@ -1,6 +1,57 @@
-use ambient_api::prelude::*;
+use ambient_api::{
+    components::core::{
+        ecs::children, primitives::cube, rendering::color, transform::local_to_parent,
+    },
+    prelude::*,
+};
+
+use components::{
+    items::*,
+    player::{left_hand_ref, local_player_ref, right_hand_ref},
+};
+use messages::UpdateHeldItems;
 
 mod shared;
 
 #[main]
-fn main() {}
+fn main() {
+    run_async(async_main());
+}
+
+async fn async_main() {
+    let local_player_entity = entity::wait_for_component(entity::resources(), local_player_ref())
+        .await
+        .expect("local_player_ref resource was deleted");
+
+    let Some(left_hand) = entity::get_component(local_player_entity, left_hand_ref()) else { panic!("local player entity has no left hand") };
+    let Some(right_hand) = entity::get_component(local_player_entity, right_hand_ref()) else { panic!("local player entity has no right hand") };
+
+    UpdateHeldItems::subscribe(move |_, data| {
+        update_held_item(left_hand, data.left);
+        update_held_item(right_hand, data.right);
+    });
+}
+
+fn update_held_item(hand: EntityId, class: EntityId) {
+    for child in entity::get_component(hand, children()).unwrap_or_default() {
+        entity::despawn_recursive(child);
+    }
+
+    if class.is_null() {
+        return;
+    }
+
+    let mut item_instance = Entity::new().with_default(local_to_parent());
+
+    if let Some(new_color) = entity::get_component(class, color()) {
+        item_instance.set(color(), new_color);
+    }
+
+    // if let Some(prefab) = entity::get_component(class, items::prefab_path()) {
+    //     item_instance.set(prefab_from_url(), asset::url(prefab).unwrap());
+    // } else {
+    item_instance.set(cube(), ());
+    // }
+
+    entity::add_child(hand, item_instance.spawn());
+}
--- src/items/server.rs
+++ src/items/server.rs
@@ -1,6 +1,39 @@
-use ambient_api::prelude::*;
+use std::collections::HashSet;
+
+use ambient_api::{components::core::player::user_id, prelude::*};
+
+use components::{
+    items::*,
+    player::{left_hand_ref, owner_ref, right_hand_ref},
+};
+use messages::UpdateHeldItems;
 
 mod shared;
 
+fn update_player_held(e: EntityId) -> Option<()> {
+    let uid = entity::get_component(e, user_id())?;
+    let left = entity::get_component(e, left_hand_ref())?;
+    let right = entity::get_component(e, right_hand_ref())?;
+
+    let left_held = entity::get_component(left, held_ref()).unwrap_or_default();
+    let right_held = entity::get_component(right, held_ref()).unwrap_or_default();
+    UpdateHeldItems::new(left_held, right_held).send_client_targeted_reliable(uid);
+
+    Some(())
+}
+
 #[main]
-fn main() {}
+fn main() {
+    change_query((held_ref(), owner_ref()))
+        .track_change(held_ref())
+        .bind(move |entities| {
+            let mut dirty_players = HashSet::new();
+            for (_, (_held, owner)) in entities {
+                dirty_players.insert(owner);
+            }
+
+            for player in dirty_players {
+                update_player_held(player);
+            }
+        });
+}
