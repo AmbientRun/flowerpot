171dec0db78391791e3aba063257c9fecee8d32d
marceline-cramer <cramermarceline@gmail.com>
2023-08-03 16:53:59 -0600

    actions: relay client-side action messages to server

--- src/actions/client.rs
+++ src/actions/client.rs
@@ -6,13 +6,7 @@ mod shared;
 
 #[main]
 fn main() {
-    Frame::subscribe(move |_| {
-        // TODO interoperate with cursor lock?
-        let (delta, _) = input::get_delta();
-
-        if delta.keys.contains(&KeyCode::Q) {
-            eprintln!("sending crafting action");
-            PerformCraftingAction::new().send_server_reliable();
-        }
+    PerformCraftingAction::subscribe(move |_, data| {
+        data.send_server_reliable();
     });
 }
