8ed48305a25964fc75f9a556aabc1b2930886f7a
marceline-cramer <cramermarceline@gmail.com>
2023-08-04 14:34:50 -0600

    ui: add name to joined players

--- src/ui/server.rs
+++ src/ui/server.rs
@@ -2,7 +2,7 @@ use ambient_api::prelude::*;
 
 mod shared;
 
-use components::fauna::fauna;
+use components::fauna;
 use messages::{AcceptJoin, JoinDenied, JoinRequest};
 
 #[main]
@@ -11,6 +11,7 @@ fn main() {
         let Some(player_entity) = source.client_entity_id() else { return };
         let Some(uid) = source.client_user_id() else { return };
         AcceptJoin::new().send_client_targeted_reliable(uid);
-        entity::add_component(player_entity, fauna(), ());
+        entity::add_component(player_entity, fauna::fauna(), ());
+        entity::add_component(player_entity, fauna::name(), data.name);
     });
 }
