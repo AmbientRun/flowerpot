2c4f00176971e0b15ac1a614ba4012870d834aef
marceline-cramer <cramermarceline@gmail.com>
2023-08-28 20:57:07 -0600

    terrain: spawn AND change queries for altitude

--- core/terrain/src/shared.rs
+++ core/terrain/src/shared.rs
@@ -33,49 +33,71 @@ pub fn init_shared_terrain() {
         }
     });
 
+    spawn_query((position(), in_chunk())).bind(move |entities| {
+        for (e, (position, in_chunk)) in entities {
+            update_altitude(e, position, in_chunk);
+        }
+    });
+
     change_query((position(), in_chunk()))
         .track_change(in_chunk())
         .bind(move |entities| {
             for (e, (position, in_chunk)) in entities {
-                let Some(chunk_xy) = entity::get_component(in_chunk, chunk()) else { continue };
-
-                let local_pos = position - (chunk_xy * CHUNK_SIZE as i32).as_vec2();
-                let coarse_pos = local_pos.floor().as_ivec2();
-                let fine_pos = local_pos - coarse_pos.as_vec2();
-
-                if coarse_pos.x < 0
-                    || coarse_pos.y < 0
-                    || coarse_pos.x >= CHUNK_SIZE as i32
-                    || coarse_pos.y >= CHUNK_SIZE as i32
-                {
-                    continue;
-                }
-
-                let Some(altitudes) = entity::get_component(in_chunk, heightmap()) else { continue };
-
-                let get_altitude = |x, y| {
-                    let x = x as usize;
-                    let y = y as usize;
-                    let idx = y * (CHUNK_SIZE + 1) + x;
-                    altitudes[idx] as f32 / 4.0
-                };
-
-                let IVec2 { x, y } = coarse_pos;
-                let v1 = get_altitude(x, y);
-                let v2 = get_altitude(x + 1, y);
-                let v3 = get_altitude(x, y + 1);
-                let v4 = get_altitude(x + 1, y + 1);
-
-                let (base, flip_x, dx, flip_y, dy) = if fine_pos.x + fine_pos.y < 1.0 {
-                    (v1, false, v2 - v1, false, v3 - v1)
-                } else {
-                    (v4, true, v3 - v4, true, v2 - v4)
-                };
-
-                let x = if flip_x { 1.0 - fine_pos.x } else { fine_pos.x };
-                let y = if flip_y { 1.0 - fine_pos.y } else { fine_pos.y };
-                let new_altitude = base + x * dx + y * dy;
-                entity::add_component(e, altitude(), new_altitude);
+                update_altitude(e, position, in_chunk);
             }
         });
+}
+
+pub fn update_altitude(e: EntityId, position: Vec2, in_chunk: EntityId) {
+    let Some(chunk_xy) = entity::get_component(in_chunk, chunk()) else {
+        return;
+    };
+
+    let local_pos = position - (chunk_xy * CHUNK_SIZE as i32).as_vec2();
+
+    let Some(altitudes) = entity::get_component(in_chunk, heightmap()) else {
+        return;
+    };
+
+    if let Some(new_altitude) = calculate_altitude(local_pos, &altitudes) {
+        entity::add_component(e, altitude(), new_altitude);
+    }
+}
+
+pub fn calculate_altitude(local_pos: Vec2, altitudes: &[i16]) -> Option<f32> {
+    let coarse_pos = local_pos.floor().as_ivec2();
+    let fine_pos = local_pos - coarse_pos.as_vec2();
+
+    if coarse_pos.x < 0
+        || coarse_pos.y < 0
+        || coarse_pos.x >= CHUNK_SIZE as i32
+        || coarse_pos.y >= CHUNK_SIZE as i32
+    {
+        return None;
+    }
+
+    let get_altitude = |x, y| {
+        let x = x as usize;
+        let y = y as usize;
+        let idx = y * (CHUNK_SIZE + 1) + x;
+        altitudes[idx] as f32 / 4.0
+    };
+
+    let IVec2 { x, y } = coarse_pos;
+    let v1 = get_altitude(x, y);
+    let v2 = get_altitude(x + 1, y);
+    let v3 = get_altitude(x, y + 1);
+    let v4 = get_altitude(x + 1, y + 1);
+
+    let (base, flip_x, dx, flip_y, dy) = if fine_pos.x + fine_pos.y < 1.0 {
+        (v1, false, v2 - v1, false, v3 - v1)
+    } else {
+        (v4, true, v3 - v4, true, v2 - v4)
+    };
+
+    let x = if flip_x { 1.0 - fine_pos.x } else { fine_pos.x };
+    let y = if flip_y { 1.0 - fine_pos.y } else { fine_pos.y };
+    let altitude = base + x * dx + y * dy;
+
+    Some(altitude)
 }
